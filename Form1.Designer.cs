a4b4f32a659bc65b0c0fc3fe170670123f0907cd
chikuwakun <tamamo0312@gmail.com>
2022-06-14 20:30:48 +0900

    なんか形になった

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -44,17 +44,17 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(510, 124);
+            this.button1.Location = new System.Drawing.Point(391, 24);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "更新";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // comboBox1
             // 
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(131, 127);
+            this.comboBox1.Location = new System.Drawing.Point(12, 24);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(373, 20);
             this.comboBox1.TabIndex = 1;
@@ -63,15 +63,15 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(129, 184);
+            this.label1.Location = new System.Drawing.Point(12, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 12);
+            this.label1.Size = new System.Drawing.Size(61, 12);
             this.label1.TabIndex = 2;
-            this.label1.Text = "label1";
+            this.label1.Text = "未選択です";
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(510, 172);
+            this.button2.Location = new System.Drawing.Point(472, 24);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 3;
@@ -109,11 +109,11 @@
             // 
             // 閉じる
             // 
-            this.閉じる.Location = new System.Drawing.Point(449, 308);
+            this.閉じる.Location = new System.Drawing.Point(561, 4);
             this.閉じる.Name = "閉じる";
             this.閉じる.Size = new System.Drawing.Size(75, 23);
             this.閉じる.TabIndex = 4;
-            this.閉じる.Text = "button3";
+            this.閉じる.Text = "最小化";
             this.閉じる.UseVisualStyleBackColor = true;
             this.閉じる.Click += new System.EventHandler(this.button3_Click);
             // 
@@ -121,7 +121,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(648, 63);
             this.Controls.Add(this.閉じる);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.label1);
